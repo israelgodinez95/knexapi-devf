3b92131f4188fd4bf1fc349e09f856cef9e4b8b3
Israel Godinez B <isra.gb47@gmail.com>
2024-01-15 23:00:15 -0600

    adding table fields

--- migrations/20240115214336_home.js
+++ migrations/20240115214336_home.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-
+    knex.schema.hasTable('users').then(function(exists) {
+        if(!exists){
+            return knex.schema.createTable('users', function (t) {
+                t.increments('id').primary()
+                t.string('first_name', 100)
+                t.string('last_name', 100)
+                t.text('bio')
+            })
+        }
+    })
 }
 
 /**

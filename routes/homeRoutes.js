4eb3879b77b23f0f7e03e16f931a26dda2ffd9c9
Israel Godinez <isra.gb47@gmail.com>
2024-01-17 10:36:28 -0600

    knex api express example final

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,7 +6,7 @@ const homeController = require('../controllers/homeController')
 
 router.post('/homes', homeController.createHome)
 router.get('/homes', homeController.findAllHomes)
-router.get('/homes/:houseId', homeController.findOneHome)
+router.get('/homes/:houseId', homeController.findOneHome) // en la ruta se envia el param con /:nombre_del_param
 router.patch('/homes/:houseId', homeController.updateOneHome)
 router.delete('/homes/:houseId', homeController.softDeleteOneHome)
 router.delete('/homes/destroy/:houseId', homeController.destroyOneHome)
